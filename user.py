7442e231567c1af2e066583153ff39d1ef479d70
KaranSinghmods <89550353+KaranSinghmods@users.noreply.github.com>
2021-12-01 22:12:19 -0600

    Added everything
    Main.py Driveer
    User Class
    Item Class
    Shopping Cart Class

--- user.py
+++ user.py
@@ -3,8 +3,6 @@ import sys
 
 from mysql_database import connect_database
 
-
-
 class User:
     # def __init__(self):
 
@@ -27,14 +25,14 @@ class User:
         connection.close()
 
     def login(self):
-        print("Logging in..", self.username)
+        # print("Logging in..", self.username)
 
         connection, cursor = connect_database()
 
-        cursor.execute("SELECT username FROM User")
+        cursor.execute("SELECT username, password FROM User")
         result = cursor.fetchall()
         for x in result:
-            if x[0] == self.username:
+            if x[0] == self.username and x[1] == self.password:
                 cursor.close()
                 connection.close()
                 return True
@@ -58,7 +56,17 @@ class User:
 
         cursor.execute(query, data)
         connection.commit()
+
+        query = "DELETE FROM Shopping_Cart WHERE username =%s"
+        data = (self.username, )
+
+        cursor.execute(query, data)
+        connection.commit()
+
         cursor.close()
         connection.close()
 
         print("Deleted.")
+
+
+
