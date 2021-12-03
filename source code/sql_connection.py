d116316ff43129995d18d78761d1396e69f5af1a
KaranSinghmods <89550353+KaranSinghmods@users.noreply.github.com>
2021-12-03 00:04:16 -0600

    Delete source code directory

--- source code/sql_connection.py
+++
@@ -1,9 +0,0 @@
-import mysql.connector
-
-db = mysql.connector.connect(
-    host = "localhost",
-    user = "root",
-    password = "Nickajack7!",
-    database = "shop"
-    )
-mycursor = db.cursor()
